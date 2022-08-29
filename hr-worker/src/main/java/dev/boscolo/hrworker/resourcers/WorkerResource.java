d39fe61ac95d2954f7d2c3f8e983ce6c7d46729b
boscolo.dev <boscolo.dev@gmail.com>
2022-08-29 10:06:40 -0300

    Ribbon load balance

--- hr-worker/src/main/java/dev/boscolo/hrworker/resourcers/WorkerResource.java
+++ hr-worker/src/main/java/dev/boscolo/hrworker/resourcers/WorkerResource.java
@@ -2,7 +2,9 @@ package dev.boscolo.hrworker.resourcers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,13 @@ import dev.boscolo.hrworker.services.WorkerService;
 
 @RestController
 @RequestMapping(value = "/workers")
-public class WokerResource {
+public class WorkerResource {
 
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerService service;
 	
@@ -27,6 +34,9 @@ public class WokerResource {
 	
 	@GetMapping(value = "{id}")
 	public ResponseEntity<WorkerDTO> findById(@PathVariable Long id){
+		
+		logger.info("PORT: "+env.getProperty("local.server.port"));
+		
 		WorkerDTO workerDTO =  service.findById(id);
 		return ResponseEntity.ok().body(workerDTO);
 	}
